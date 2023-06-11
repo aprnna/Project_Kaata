a9e9725847c04ed493b6598c939937b002710ef0
Apriana <aprianacahya04@gmail.com>
2023-06-11 23:28:44 +0700

    update kirim.php

--- koneksi.php
+++ koneksi.php
@@ -1,4 +1,5 @@
 <?php
+# $mysqli = new mysqli("localhost","username","password","nama_database");
 $koneksi = mysqli_connect("localhost", "id20230782_sebuahkata","ASD213W@#s","id20230782_katakata");
 if (mysqli_connect_errno()){
   echo "koneksi gagal" .mysqli_connect_errno(); 
